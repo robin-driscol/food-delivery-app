92451786a42e154607a2ab9092ec5be851902021
Rayhan Farhan <61746741+robin-driscol@users.noreply.github.com>
2022-02-05 23:35:40 +0600

    Initial Commit

---
+++ components/PizzaList.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard"
+
+const PizzaList = ({pizzaList}) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Welcome to Pizzario where you get the best pizzas straight into your doorstep. Order Now!
+      </p>
+      <div className={styles.wrapper}>
+         {pizzaList.map((pizza) => (
+           <PizzaCard key={pizza._id} pizza = {pizza}/>
+         ))}
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
